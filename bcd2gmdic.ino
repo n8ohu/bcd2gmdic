b8d8ccedff843ae23a2ec8f591af945c89513c78
Jared Szechy <jared.szechy@gmail.com>
2014-06-23 22:38:43 -0400

    Add LICENSE

--- bcd2gmdic.ino
+++ bcd2gmdic.ino
@@ -1,3 +1,21 @@
+/**
+ * bcd2gmdic.ino - BCD396XT to GM DIC Arduino firmware
+ * Copyright (C) 2014 Jared Szechy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <MCP2515.h>
 #include <SoftwareSerial.h>
 
